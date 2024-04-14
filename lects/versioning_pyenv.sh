b6b272d6b2cd1b5ba3e0ac306b6110c0c09b2067
Oksana Kaledina <kaledinaoa@gmail.com>
2024-04-14 13:26:53 +0300

    add bash from lecs

---
+++ lects/versioning_pyenv.sh
@@ -0,0 +1,21 @@
+  $ pyenv versions
+    2.7.10
+  * 3.5.0 (set by /Users/yuu/.pyenv/version)
+    miniconda3-3.16.0
+    pypy-2.6.0
+
+  $ python --version
+  Python 3.6.0
+
+  $ pyenv global pypy-2.6.0
+
+  $ python --version
+  Python 2.7.9 [PyPy 2.6.0 with GCC 4.9.2]
+
+  $ cd /Volumes/treasuredata/jupyter
+
+  $ pyenv version
+  miniconda3-3.16.0 (set by /Volumes/treasuredata/.python-version)
+
+  $ python --version
+  Python 3.4.3 :: Continuum Analytics, Inc.
